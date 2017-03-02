4e98da08b6e0be272e5ffe057b54d2ffbd4da1ed
Jessica E Mizzi <jemizzi@peloton.cse.ucdavis.edu>
2017-03-01 16:16:43 -0800

    first commit

---
+++ interleave-reads.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#
+#SBATCH --job-name=interleave-reads.sh.isolate
+#SBATCH -t 2:00:00
+#SBATCH --mem=32768
+#SBATCH -o /home/jemizzi/rotation-project/scripts/error-output-reports/interleave-reads.sh.isolate.out
+#SBATCH -e /home/jemizzi/rotation-project/scripts/error-output-reports/interleave-reads.sh.isolate.err
+#SBATCH --mail-user=jemizzi@ucdavis.edu
+#SBATCH --mail-type=ALL
+
+module load python khmer
+
+for filename in /home/jemizzi/rotation-project/raw-data/isolate/*_R1_*.fastq
+do
+     # first, make the base by removing .extract.fastq.gz
+     base=$(basename $filename .fastq)
+     echo $base
+
+     # now, construct the R2 filename by replacing R1 with R2
+     baseR2=${base/_R1_/_R2_}
+     echo $baseR2
+
+     # construct the output filename
+     output=${base/_R1_/}.fastq
+
+     (interleave-reads.py /home/jemizzi/rotation-project/raw-data/isolate/${base}.fastq /home/jemizzi/rotation-project/raw-data/isolate/${baseR2}.fastq | \
+         gzip > /home/jemizzi/rotation-project/quality-control/isolate/trimmomatic-output/$output)
+
+done
---
+++ megahit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#
+#SBATCH --job-name=megahit.sh.metagenome
+#SBATCH -t 10:00:00
+#SBATCH --mem=32768
+#SBATCH -o /home/jemizzi/rotation-project/scripts/error-output-reports/megahit.sh.metagenome.out
+#SBATCH -e /home/jemizzi/rotation-project/scripts/error-output-reports/megahit.sh.metagenome.err
+#SBATCH --mail-user=jemizzi@ucdavis.edu
+#SBATCH --mail-type=ALL
+
+module load megahit
+
+megahit --12 /home/jemizzi/rotation-project/raw-data/metagenomic/paired-qc-filtered-megan/10.4_ABC_4.cat.ereps.afu.fastq -o /home/jemizzi/rotation-project/assembly/metagenome/megahit-assembly

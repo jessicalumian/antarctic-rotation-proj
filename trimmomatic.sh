a4706681f729cb8451f745a4962a14bdb2714085
Jessica Mizzi <jessica.mizzi@gmail.com>
2017-03-02 17:18:35 -0800

    trimmomatic updates

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -11,7 +11,7 @@
 module load trimmomatic
 
 # Get Illumina Adapters
-# wget link here
+wget https://anonscm.debian.org/cgit/debian-med/trimmomatic.git/plain/adapters/TruSeq3-PE-2.fa
 
 for filename in *_R1_*.fastq
 do
@@ -20,17 +20,25 @@ do
      baseR2=${base/_R1_/_R2_}
      echo $baseR2
 
-     java -jar $TRIM/trimmomatic PE -phred33 ${base}.fastq ${baseR2}.fastq \
-     intermediate/${base}.fastq intermediate/s1_se \
-     intermediate/${baseR2}.fastq intermediate/s2_se \
-     ILLUMINACLIP:TruSeq3-PE.fa:2:40:15 \
-     LEADING:2 TRAILING:2 \
-     SLIDINGWINDOW:4:2 \
+     java -jar $TRIM/trimmomatic PE -phred33 \
+     # input
+     /home/jemizzi/rotation-project/raw-data/isolate/${base}.fastq \
+     /home/jemizzi/rotation-project/raw-data/isolate/${baseR2}.fastq \
+     # output
+     /home/jemizzi/rotation-project/quality-control/isolate/trimmomatic-output/${base}.fastq \
+     /home/jemizzi/rotation-project/quality-control/isolate/trimmomatic-output/s1_se \
+     /home/jemizzi/rotation-project/quality-control/isolate/trimmomatic-output/${baseR2}.fastq \
+     /home/jemizzi/rotation-project/quality-control/isolate/trimmomatic-output/s2_se \
+     # parameters - based on suggestions from user manual
+     ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10 \
+     LEADING:3 TRAILING:3 \
+     SLIDINGWINDOW:4:15 \
      MINLEN:25
 
      # save the orphans
-     gzip -9c intermediate/s{1,2}_se >> intermediate/orphans.fq.gz
-     rm -f intermediate/s{1,2}_se
+     gzip -9c /home/jemizzi/rotation-project/quality-control/isolate/trimmomatic-output/s{1,2}_se >> /home/jemizzi/rotation-project/quality-control/isolate/trimmomatic-output/orphans.fq.gz
+     rm -f /home/jemizzi/rotation-project/quality-control/isolate/trimmomatic-output/s{1,2}_se
 
 done
-fastqc /home/jemizzi/rotation-project/raw-data/metagenomic/*
+
+rm TruSeq3-PE-2.fa

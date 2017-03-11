f904ac8f964618d6206bc0d093f42565389baf33
Jessica Mizzi <jessica.mizzi@gmail.com>
2017-03-11 13:01:26 -0800

    start anvi viz script, map on fixed contigs

---
+++ anvio-visualization-prep.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+#
+#SBATCH --job-name=anvio-visualization-prep.sh.metagenome
+#SBATCH -t 10:00:00
+#SBATCH --mem=32768
+#SBATCH -o /home/jemizzi/rotation-project/scripts/error-output-reports/anvio-visualization-prep.sh.metagenome.out
+#SBATCH -e /home/jemizzi/rotation-project/scripts/error-output-reports/anvio-visualization-prep.sh.metagenome.err
+#SBATCH --mail-user=jemizzi@ucdavis.edu
+#SBATCH --mail-type=ALL
+
+module load bio/1.0 anvio/2.0.2
+
+# Assembly fasta and BAM file in anvio directory
+
+# Generate anvi'o contigs database
+anvi-gen-contigs-database -f /home/jemizzi/rotation-project/visualization-anvio/metagenome/contigs-fixed.fa -o contigs.db
+
+# Generate HMMS of database
+anvi-run-hmms -c contigs.db
+
+# Annotate Taxonomy
+# need to have centrifuge downloaded onto peloton
+
+# Annotate functions
+# need to annotate functions first
+
+# Profiling BAM files
+# Can skip this step of bam file is indexed - included here only for reference
+# anvi-init-bam SAMPLE-01-RAW.bam -o SAMPLE-01.bam
+
+# Creating anvi-profile - incorporates stats from BAM file
+anvi-profile -i metagenome-mapped-fixed-contigs.sorted.bam -c contigs.db
+
+# Merge anvio profiles - necessary if working with multiple samples and bam files, not this situation but kept here for reference
+
+# Start interactive portion -- tree file is necessary, but should have been generated in an earlier step. 
+
+
--- bwa2-mapping.sh
+++ bwa2-mapping.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 #
-#SBATCH --job-name=bwa2-mapping.sh.metagenome
+#SBATCH --job-name=bwa2-mapping.sh.metagenome-fixed-contigs
 #SBATCH -t 10:00:00
 #SBATCH --mem=32768
-#SBATCH -o /home/jemizzi/rotation-project/scripts/error-output-reports/bwa2-mapping.sh.metagenome.out
-#SBATCH -e /home/jemizzi/rotation-project/scripts/error-output-reports/bwa2-mapping.sh.metagenome.err
+#SBATCH -o /home/jemizzi/rotation-project/scripts/error-output-reports/bwa2-mapping.sh.metagenome-fixed-contigs.out
+#SBATCH -e /home/jemizzi/rotation-project/scripts/error-output-reports/bwa2-mapping.sh.metagenome-fixed-contigs.err
 #SBATCH --mail-user=jemizzi@ucdavis.edu
 #SBATCH --mail-type=ALL
 
@@ -12,18 +12,18 @@ module load bowtie2/2.2.5 samtools/1.3.1
 set -e
 
 #index reference
-bwa index /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output/final.contigs.fa
+bwa index /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output-fixed-contigs-anvio/contigs-fixed.fa
 # map raw reads to reference assembly
-bwa mem /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output/final.contigs.fa /home/jemizzi/rotation-project/raw-data/metagenomic/paired-qc-filtered-megan/10.4_ABC_4.cat.ereps.afu.fastq > /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output/metagenome-mapped.sam
+bwa mem /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output-fixed-contigs-anvio/contigs-fixed.fa /home/jemizzi/rotation-project/raw-data/metagenomic/paired-qc-filtered-megan/10.4_ABC_4.cat.ereps.afu.fastq > /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output-fixed-contigs-anvio/metagenome-mapped-fixed-contigs.sam
 
 # index reference assembly with samtools
-samtools faidx /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output/final.contigs.fa
+samtools faidx /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output-fixed-contigs-anvio/contigs-fixed.fa
 
 # convert sam to bam
-samtools import /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output/contigs.fa.fai /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output/metagenome-mapped.sam /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output/metagenome-mapped.bam
+samtools import /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output-fixed-contigs-anvio/contigs.fa.fai /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output-fixed-contigs-anvio/metagenome-mapped-fixed-contigs.sam /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output-fixed-contigs-anvio/metagenome-mapped-fixed-contigs.bam
 
 # sort the bam file
-samtools sort /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output/metagenome-mapped.bam > /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output/metagenome-mapped.sorted.bam
+samtools sort /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output-fixed-contigs-anvio/metagenome-mapped-fixed-contigs.bam > /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output-fixed-contigs-anvio/metagenome-mapped-fixed-contigs.sorted.bam
 
 # index the sorted bam file
-samtools index /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output/metagenome-mapped.sorted.bam
+samtools index /home/jemizzi/rotation-project/mapping/metagenome/bwa2-samtools-output-fixed-contigs-anvio/metagenome-mapped-fixed-contigs.sorted.bam

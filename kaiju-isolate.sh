29f1e3663ffbe6802dc788239b939b55ca6beef9
Jessica Mizzi <jessica.mizzi@gmail.com>
2017-03-15 13:52:55 -0700

    add kaiju scripts

---
+++ kaiju-isolate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#
+#SBATCH --job-name=kaiju-isolate.sh.isolate
+#SBATCH -t 10:00:00
+#SBATCH --mem=32768
+#SBATCH -o /home/jemizzi/rotation-project/scripts/error-output-reports/kaiju-isolate.sh.isolate.out
+#SBATCH -e /home/jemizzi/rotation-project/scripts/error-output-reports/kaiju-isolate.sh.isolate.err
+#SBATCH --mail-user=jemizzi@ucdavis.edu
+#SBATCH --mail-type=ALL
+
+module load kaiju/1
+
+kaiju -i /home/jemizzi/rotation-project/databases/kaiju-database/nodes.dmp /home/jemizzi/rotation-project/databases/kaiju-database/kaiju_db_nr_euk.fmi -i /home/jemizzi/rotation-project/assembly/isolate/megahit-assembly/final.contigs.fa

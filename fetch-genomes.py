168d64c4e47d3d42365d5cbc484d70a441ca87ca
Jessica Mizzi <jessica.mizzi@gmail.com>
2017-04-05 17:13:29 -0700

    add fetch genome commands :D

---
+++ fetch-genomes.py
@@ -0,0 +1,29 @@
+# script from https://github.com/adina/isu-bcb-546x/blob/master/scripts/fetch-genomes.py
+
+import urllib2
+import os
+import sys
+import time
+
+if len(sys.argv) != 3:
+    print "USAGE: fetch-genomes.py <genome_id_list> <out_dir>"
+    sys.exit(1)
+
+url_template = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=%s&rettype=fasta&retmode=text"
+
+os.mkdir(sys.argv[2])
+
+for id in open(sys.argv[1]):
+    id = id.strip()
+    if id == "":
+        continue
+
+    sys.stdout.write("Fetching %s..." % id)
+    sys.stdout.flush()
+    out_file = os.path.join(sys.argv[2], id + ".fa")
+    if os.path.exists(out_file):
+        print "already fetched"
+
+    open(out_file, "w").write(urllib2.urlopen(url_template % id).read())
+    print "Done"
+    time.sleep(1.0/3)
